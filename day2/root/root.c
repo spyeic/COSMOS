c0c0e5df75ad1fb1bcbc58db786decfd060ae715
Eric <lderic0415@gmail.com>
2023-07-11 21:50:52 -0700

    decrease iterate times

--- day2/root/root.c
+++ day2/root/root.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    int j;
+int main()
+{
+    int j, n;
     double x, A;
     printf("Enter x:   ");
     scanf("%lf", &x);
     printf("Enter A:   ");
     scanf("%lf", &A);
-    for (j = 0; j < 20; j++) {
+    printf("Attempts:   ");
+    scanf("%d", &n);
+    int prev;
+    for (j = 0; j < n; j++)
+    {
+        prev = x;
         x = x / 2. + A / (2. * x);
+        if (x == prev)
+        {
+            break;
+        }
         printf("%5i %12.8lf\n", j, x);
     }
     return 0;
