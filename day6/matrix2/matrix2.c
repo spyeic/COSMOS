b87ba801018862cf11e5131ba3c1a36e7f8616a1
spyeic <lderic0415@gmail.com>
2023-07-21 10:06:50 -0700

    finish matrix2

--- day6/matrix2/matrix2.c
+++ day6/matrix2/matrix2.c
@@ -16,10 +16,38 @@ int main()
         }
     }
 
-    printf("enter matrix for v:");
+    printf("enter matrix for v:\n");
 
     for (int i = 0; i < N; i++)
     {
         scanf("%lf", &v[i]);
     }
+
+    printf("Added result: \n");
+    double M_added[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            M_added[i][j] = M[i][j] * 2;
+            printf("%lf ", M_added[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Multiply M by v\n");
+    double Mv[N];
+    for (int i = 0; i < N; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < N; j++)
+        {
+            sum += v[j] * M[i][j];
+        }
+        Mv[i] = sum;
+        printf("%lf ", sum);
+    }
+
+    printf("\n");
+    return 0;
 }
