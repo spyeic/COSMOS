35474b50396f15e3da7b79039ecbb632e0abbd9e
Eric <lderic0415@gmail.com>
2023-07-27 11:31:57 -0700

    add lab13

--- lab13/lab13.c
+++ lab13/lab13.c
@@ -1,30 +1,63 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "../matrix_functions.h"
 
-Mat * create_H(int N)
+#define E0 -13.6
+#define Ehop 0.5
+
+void create_H(Mat *H)
 {
-    Mat * H;
-    makeMatrix(N, N, H);
-    for (size_t i = 0; i < N; i++)
+    for (int i = 0; i < H->N; i++)
     {
-        for (size_t j = 0; j < N; j++)
+        for (int j = 0; j < H->M; j++)
         {
             if (i == j)
             {
-                H->Matrix[i][j] = -13.6;
+                H->Matrix[i][j] = E0;
             }
             if (i == j + 1 || i == j - 1)
             {
-                H->Matrix[i][j] = 0.5;
+                H->Matrix[i][j] = Ehop;
             }
         }
-        
-    }
-    printMatrix(H);
-    return H;
+        }
 }
 
 int main()
 {
-    create_H(5);
+    int N = 512;
+    int i, j;
+    Mat testmat, vecs;
+    Vec vals_real, vals_im;
+
+    makeMatrix(N, N, &testmat);
+    makeMatrix(N, N, &vecs);
+    makeVector(N, &vals_real);
+    makeVector(N, &vals_im);
+
+    create_H(&testmat);
+
+    get_eigenvalues(&testmat, &vals_real, &vals_im, &vecs, 1);
+
+    //  PRINT TO SCREEN:
+
+    printf("\n Original Matrix being diagonalized :\n");
+    printMatrix(&testmat);
+
+    printf("\n Real part of Eigenvalues :\n");
+    printVector(&vals_real);
+
+    printf("\n Imaginary part of Eigenvalues :\n");
+    printVector(&vals_im);
+
+    printf("\n Eigenvectors (are *rows*):\n");
+    printMatrix(&vecs);
+
+    FILE * fileout = fopen("eigenvalues512.data", "w");
+
+    for (i = 0; i < N; i++)
+    {
+        fprintf(fileout, "%4i %8.4lf \n", i,vals_real.Vector[i]);
+    }
+    return 0;
 }
